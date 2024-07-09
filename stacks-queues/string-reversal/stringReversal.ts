6e737d85dd58299975b371ed63b057db37831aaa
Anya Aven <leafeon96@gmail.com>
2024-07-08 20:55:43 -0400

    Finished stringReversal - BUT it can be refactored
    Why use a StackStr and not an array?

--- stacks-queues/string-reversal/stringReversal.ts
+++ stacks-queues/string-reversal/stringReversal.ts
@@ -3,7 +3,18 @@ import { StackStr } from "../common/stack";
 /** Return reverse of string. */
 
 function stringReversal(str: string): string {
-  return "x";
+
+  const stack = new StackStr();
+  let reversedStr = "";
+
+  for(const ltr of str){
+    stack.push(ltr)
+  }
+  for(const ltr of str){
+    reversedStr += stack.pop()
+  }
+
+  return reversedStr
 }
 
 export { stringReversal };
