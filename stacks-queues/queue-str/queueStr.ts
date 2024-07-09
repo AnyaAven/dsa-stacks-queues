2cc4dc068113b614ccb29997879be0ff9ed879ca
Jacob <jacobrashid404@gmail.com>
2024-07-08 17:37:14 -0700

    implement Queue with Linked List

--- stacks-queues/queue-str/queueStr.ts
+++ stacks-queues/queue-str/queueStr.ts
@@ -1,28 +1,34 @@
 /** QueueStr: can remove from start or add to end */
+import { LLStr } from "../common/ll";
 
 class QueueStr {
+  _ll: LLStr;
 
   constructor(initial: string[] = []) {
+    this._ll = new LLStr(initial);
   }
 
   /** enqueue(val): add val to end. Returns undefined. */
   enqueue(val: string): void {
+    this._ll.push(val);
   }
 
   /** dequeue(): remove & return item from start.
    * Throws error if empty. */
   dequeue(): string {
-    return "x";
+    if(this._ll.length === 0) throw new Error;
+    return this._ll.shift();
   }
 
   /** peek(): return the value of top. */
   peek(): string {
-    return "x";
+    if(!this._ll.head) throw new Error;
+    return this._ll.head.val;
   }
 
   /** isEmpty(): is the queue empty? */
   isEmpty(): boolean {
-    return false;
+    return this._ll.length === 0;
   }
 }
 
